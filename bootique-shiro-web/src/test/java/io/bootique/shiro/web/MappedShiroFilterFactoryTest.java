557a93d250f039f1c12a4864c07fc578f362293d
Andrus Adamchik <andrus@objectstyle.com>
2018-04-07 15:15:11 +0300

    deprecated API cleanup

--- bootique-shiro-web/src/test/java/io/bootique/shiro/web/MappedShiroFilterFactoryTest.java
+++ bootique-shiro-web/src/test/java/io/bootique/shiro/web/MappedShiroFilterFactoryTest.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 
