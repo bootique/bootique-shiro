56ee3ff2c722d4b04887778ac9321479bc41dc8c
Andrus Adamchik <andrus@objectstyle.com>
2024-05-25 08:37:35 -0400

    cleanup: no need for empty init/destroy in Filter anymore

--- bootique-shiro-web-jakarta-mdc/src/main/java/io/bootique/shiro/web/mdc/SubjectMDCInitializer.java
+++ bootique-shiro-web-jakarta-mdc/src/main/java/io/bootique/shiro/web/mdc/SubjectMDCInitializer.java
@@ -40,10 +40,6 @@ public class SubjectMDCInitializer implements Filter {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
